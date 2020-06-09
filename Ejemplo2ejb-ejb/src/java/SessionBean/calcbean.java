de3bef08eb3c1ee0c2087742eb5a195a0f266426
ManONES <manuel.romero@siessoftware.net>
2020-06-09 14:38:32 -0500

    sffff

--- Ejemplo2ejb-ejb/src/java/SessionBean/calcbean.java
+++ Ejemplo2ejb-ejb/src/java/SessionBean/calcbean.java
@@ -6,6 +6,10 @@
 package SessionBean;
 
 import javax.ejb.Stateless;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.ws.rs.GET;
 
 /**
  *
@@ -21,5 +25,40 @@ public class calcbean implements calcbeanLocal {
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
+    
+    @Override
+    @GET
+    public JsonArray dukes(){
+        return Json.createArrayBuilder().
+                add(duke("smart",18)).
+                add(duke("nice",42)).
+                add(duke("many",55)).
+                add(duke("Harry",17)).
+                build();
+    }
+       
+    @Override
+    public JsonObject duke (String name, int age){
+        return Json.createObjectBuilder().
+                add("name", name).
+                add("age", age).
+                build();
+    }
+
+
+    @Override
+    @GET
+    public JsonArray mensajes(){
+        return Json.createArrayBuilder().
+                add(mensaje("data received")).
+                build();
+    }
+       
+    @Override
+    public JsonObject mensaje (String message){
+        return Json.createObjectBuilder().
+                add("message", message).
+                build();
+    }
     
 }
